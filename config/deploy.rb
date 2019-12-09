68c66b13bdffd783905852446cfa37d8a2e88e15
ckr344 <ckr344.225@gmail.com>
2019-12-09 20:07:12 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :repo_url,  'git@github.com:ckr344/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
@@ -51,5 +52,3 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-
-set :linked_files, %w{ config/secrets.yml }
